a0a2742bef56a774d02f45714bf1ead6cd3aa8a8
Aman Pandey <amanpandey123@gmail.com>
2026-09-05 12:01:49 +0530

    Set frontend API baseURL to relative /api to enforce Vercel serverless execution

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,10 +1,7 @@
 import axios from "axios";
 
-const rawApiUrl = import.meta.env.VITE_API_URL;
-const isValidUrl = rawApiUrl && rawApiUrl.startsWith("http");
-
 const api = axios.create({
-  baseURL: isValidUrl ? rawApiUrl : "https://medremind-iaeu.onrender.com/api",
+  baseURL: "/api",
   withCredentials: true,
 });
 
